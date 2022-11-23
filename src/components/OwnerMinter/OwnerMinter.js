095091a80df0da1aa6afec7dc8f9ae0e217fc6fe
Anvesh-Thakur <anvesh@nfthing.com>
2022-11-23 10:52:38 +0530

    Anvesh | Added Basic QR Scanner

--- src/components/OwnerMinter/OwnerMinter.js
+++ src/components/OwnerMinter/OwnerMinter.js
@@ -1,8 +1,21 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
+import Html5QrcodePlugin from './Html5QrcodePlugin.jsx'
+// import {QrReader}  from 'react-qr-reader';
 
 const OwnerMinter = ({claimNft}) => {
   
+  const [Scan, setScan] = useState(false);
+  const [result, setResult] = useState('');  
   const address = useRef("");
+
+  const previewStyle = {
+    "height": "240px",
+    "width": "240px",
+  }
+
+  const onNewScanResult = (decodedText, decodedResult) => {
+    console.log(decodedText)
+  }
 
   return (
     <div className="App">
@@ -11,6 +24,30 @@ const OwnerMinter = ({claimNft}) => {
         address: <input type="text" ref={address} />
         <br />
         <button onClick={() => claimNft(address.current.value)}>Send To!</button>
+        <br />
+        <button onClick={() => setScan(prev => !prev)}>
+          Scan
+        </button>
+
+        {/* {Scan && 
+          <div style={previewStyle}>
+          <QrReader 
+          delay={300}
+          onResult={(result, error) => {
+            !!result && console.log(result?.text);
+            !!error && console.log(error);
+        }}
+        />
+        </div>
+        }  */}
+
+          <Html5QrcodePlugin 
+                fps={10}
+                qrbox={250}
+                disableFlip={false}
+                qrCodeSuccessCallback={onNewScanResult}/>  
+
+
       </header>
     </div>
   )
