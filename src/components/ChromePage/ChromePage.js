19ab32182ea0d33e778225b4500fcb85d9cf126d
vishalhanuman <1by19cs182@bmsit.in>
2022-12-07 18:39:48 +0530

    Vishal | Mobile chrome browser fixes Copy button working Added socials

--- src/components/ChromePage/ChromePage.js
+++ src/components/ChromePage/ChromePage.js
@@ -1,51 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import nfthing from "../../logos/Group 1.png";
 import "./ChromePage.css";
+import { BsTwitter, BsInstagram } from "react-icons/bs";
 
 const ChromePage = () => {
   const [copied, setCopied] = useState(false);
-  const copy = () => {
-    navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
-      if (result.state == "granted" || result.state == "prompt") {
-        alert("Write access ranted!");
-      } else {
-        alert("not granted");
+
+  useEffect(() => {
+    Copy();
+  }, []);
+  const Copy = () => {
+    var copyTextareaBtn = document.querySelector(".copy-button");
+
+    copyTextareaBtn.addEventListener("click", function (event) {
+      var copyTextarea = document.querySelector(".copy-text");
+      copyTextarea.focus();
+      copyTextarea.select();
+
+      try {
+        var successful = document.execCommand("copy");
+        var msg = successful ? "successful" : "unsuccessful";
+        console.log("Copying text command was " + msg);
+        setCopied(true);
+      } catch (err) {
+        console.log("Oops, unable to copy");
       }
     });
-    navigator.clipboard.writeText("minting.nfthing.com");
-    setCopied(true);
   };
-
-  function copyToClipboard(string) {
-    let textarea;
-    let result;
-
-    try {
-      textarea = document.getElementById("copy-text");
-      textarea.setAttribute("readonly", true);
-      textarea.setAttribute("contenteditable", true);
-      // textarea.style.position = "fixed"; // prevent scroll from jumping to the bottom when focus is set.
-      textarea.value = string;
-
-      document.body.appendChild(textarea);
-
-      textarea.focus();
-      textarea.select();
-
-      const range = document.createRange();
-      range.selectNodeContents(textarea);
-
-      const sel = window.getSelection();
-      sel.removeAllRanges();
-      sel.addRange(range);
-
-      textarea.setSelectionRange(0, textarea.value.length);
-      result = document.execCommand("copy");
-    } catch (err) {
-      console.error(err);
-      result = null;
-    }
-  }
 
   return (
     <>
@@ -64,12 +45,11 @@ const ChromePage = () => {
             </h3>
           </div>
           <div>
-            <div className="copy-field" id="copy-text">
-              <h4>https://minter.nfthing.com</h4>
-              <div
-                // onClick={copyToClipboard("https://rit.nfthing.com/")}
-                className="copy-button"
-              >
+            <div className="copy-field">
+              <textarea className="copy-text" readOnly>
+                rit.nfthing.com
+              </textarea>
+              <div className="copy-button">
                 <h4>{copied ? "Copied" : "Copy link"}</h4>
               </div>
             </div>
@@ -78,15 +58,28 @@ const ChromePage = () => {
         <div className="metamask">
           <a
             className="deeplink"
-            href="https://metamask.app.link/dapp/rit.nfthing.com/"
+            href="https://metamask.app.link/dapp/ritform.nfthing.com/"
           >
             OPEN METAMASK
           </a>
         </div>
-        <div className="chrome-socials">
-          <h3>generating links</h3>
-          <h3>Follow us on</h3>
-          <h3> Twitter Instagram</h3>
+        <div className="socials mob">
+          <div class="gentext">
+            {" "}
+            //GENERATING <br></br> SOCIAL LINKS FROM INDEX
+          </div>
+          <h2>FOLLOW US ON</h2>
+          <div className="links">
+            <a href="https://twitter.com/NFThing_" target="_blank">
+              <BsTwitter className="icon" /> <p>TWITTER</p>
+            </a>
+            <a
+              href="https://instagram.com/nfthing_?igshid=YmMyMTA2M2Y="
+              target="_blank"
+            >
+              <BsInstagram className="icon" /> <p>INSTAGRAM</p>
+            </a>
+          </div>
         </div>
       </div>
     </>
